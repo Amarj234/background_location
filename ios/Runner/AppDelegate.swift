6c13b64d18eeffffac3e71b6180927e75cd49299
macbookair <amarj234@gmail.com>
2025-08-09 17:42:26 +0530

    ios add

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,28 @@
 import Flutter
 import UIKit
+import CoreLocation
 
-@main
-@objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate {
+    var locationManager: CLLocationManager?
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.requestAlwaysAuthorization()
+        locationManager?.allowsBackgroundLocationUpdates = true
+        locationManager?.pausesLocationUpdatesAutomatically = false
+        locationManager?.startUpdatingLocation()
+        return true
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            print("Lat: \(location.coordinate.latitude), Lng: \(location.coordinate.longitude)")
+            // Save to UserDefaults so Flutter can read it later
+            UserDefaults.standard.set(location.coordinate.latitude, forKey: "last_lat")
+            UserDefaults.standard.set(location.coordinate.longitude, forKey: "last_lng")
+        }
+    }
 }
